7ee6d5edd95d2ce4aa267786f7e4dc023204fa26
Tobi <blackmanmaker@gamil.com>
2025-11-22 07:34:30 +0100

    korrektur

--- worker.js
+++ worker.js
@@ -8,10 +8,26 @@ class Worker {
     this.name = name;
     this.grid = [];
     
+    // Hinzufügen von Typüberprüfungen
+    this.validateProperties();
   }
 
-  
+  validateProperties() {
+    if (typeof this.x !== 'number' || typeof this.y !== 'number') {
+      throw new Error('x und y müssen Zahlen sein');
+    }
+    if (typeof this.width !== 'number' || typeof this.height !== 'number') {
+      throw new Error('width und height müssen Zahlen sein');
+    }
+    if (typeof this.color !== 'string') {
+      throw new Error('color muss ein String sein');
+    }
+    if (typeof this.name !== 'string') {
+      throw new Error('name muss ein String sein');
+    }
+  }
 
+  // Hier könnten weitere Methoden hinzugefügt werden
 }
 
 export default Worker;
